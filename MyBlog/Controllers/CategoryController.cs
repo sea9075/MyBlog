0451f3c2a09ea462f41cfd99dce3a49322a313cb
sea9075 <65601991+sea9075@users.noreply.github.com>
2024-07-30 23:55:51 +0800

    20240730_addConnectionAndCategoryReadAndCreate

---
+++ MyBlog/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Data;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyBlog/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBlog.Models
+{
+    public class Category
+    {
+        [Key]
+        [DisplayName("類別序號")]
+        public int CategoryId { get; set; }
+        [Required]
+        [DisplayName("類別名稱")]
+        public string CategoryName { get; set; }
+    }
+}
